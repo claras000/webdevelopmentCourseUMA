d407aaef138c07785b004d606c5e9b703faff266
claras000 <clara.o.correa@web.de>
2024-04-14 15:25:10 +0100

    cheking index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
